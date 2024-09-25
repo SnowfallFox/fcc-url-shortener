bc5c12d434ce55f9b452aeb5bbb168cf14bf866c
SnowfallFox <icebornwhisker@gmail.com>
2024-09-25 13:35:48 +0000

    feat: added rudimentary error handling for invalid URL formats

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 //
 const dns = require('dns');
 const bodyParser = require('body-parser');
+const options = {
+  all:true,
+};
+
+//
+const query_regex = /^(https?:\/\/)/gmi
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -13,6 +19,9 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+//
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -20,6 +29,24 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+//
+app.route('/api/shorturl').post((req,res) => {
+  // console.log(req.body.url)
+  if (query_regex.test(req.body.url)) {
+    let query = req.body.url.replace(query_regex, "")
+    dns.lookup(query, (error,addresses) => {
+      console.log(`${req.body.url} = ${query} = error: ${error}, address: ${addresses}`)
+      if (error !== null) {
+       res.json({'error':'invalid url'})
+      } else {
+        res.json({'no':'error'})
+      }
+    });
+  } else {
+    res.json({'error': 'invalid url'})
+  }
 });
 
 app.listen(port, function() {
